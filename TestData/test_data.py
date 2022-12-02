b3d50dd34d6fc7bf52a32ad5da5272ec83bbb85b
Digvijayautomation <dtikka6@gmail.com>
2022-12-02 18:42:53 +0530

    "Framework with report"

---
+++ TestData/test_data.py
@@ -0,0 +1,16 @@
+class test_data:
+
+ # 2 sets of data one is valid credentials, second is invalid credentials
+    testdata=[{"username": "Admin",
+               "password":"admin123",
+               "fname":"Digvijay",
+               "mname":"Tukaram",
+               "lname":"Tikka"
+               },
+              {"username": "xyz",
+               "password": "admin123",
+               "fname": "Digvijay",
+               "mname": "Tukaram",
+               "lname": "Tikka"
+               }
+              ]
