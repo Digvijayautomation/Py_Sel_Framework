f55e798764c8c1548089d6855407277d0cd5e195
Digvijayautomation <dtikka6@gmail.com>
2023-02-27 16:49:52 +0530

    Merge remote-tracking branch 'origin/master'

--- TestData/test_data.py
+++ TestData/test_data.py
@@ -1,5 +1,5 @@
 class test_data:
-    # 2 sets of data one is valid credentials, second is invalid credentials
+
     testdata = [
                 {
                  "username": "Admin",
@@ -8,11 +8,5 @@ class test_data:
                  "mname": "Tukaram",
                  "lname": "Tikka"
                  },
-                    {
-                 "username": "Admin122",
-                 "password": "admin123",
-                 "fname": "Digvijay",
-                 "mname": "Tukaram",
-                 "lname": "Tikka"
-                 }
+
                 ]
